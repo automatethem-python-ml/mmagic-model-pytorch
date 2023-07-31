5670b137994c731895b090944b3444bc2d262a4f
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-07-31 23:19:24 +0900

    Update video_super_resolution.py

--- video_super_resolution.py
+++ video_super_resolution.py
@@ -1,1 +1,10 @@
+import os
+from mmagic.apis import MMagicInferencer
+from mmengine import mkdir_or_exist
 
+# Create a MMagicInferencer instance and infer
+video = 'predict_inputs/narrator_speech.mp4'
+result_out_dir = 'predict_outputs/narrator_speech_2x.mp4'
+mkdir_or_exist(os.path.dirname(result_out_dir))
+editor = MMagicInferencer('basicvsr')
+results = editor.infer(video=video, result_out_dir=result_out_dir)
