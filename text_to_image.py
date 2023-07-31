97dc1210fc05d88bd3cafe36782414b767116a6a
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-08-01 00:39:32 +0900

    Add files via upload

---
+++ text_to_image.py
@@ -0,0 +1,5 @@
+from mmagic.apis import MMagicInferencer
+sd_inferencer = MMagicInferencer(model_name='stable_diffusion')
+text_prompts = 'A panda is having dinner at KFC'
+result_out_dir = 'output/sd_res.png'
+sd_inferencer.infer(text=text_prompts, result_out_dir=result_out_dir)
